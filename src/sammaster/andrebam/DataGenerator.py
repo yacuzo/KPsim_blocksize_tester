a9553925be83a3f9381435f1d6be2f155541a2d5
yacuzo <blackcrow@getmail.no>
2013-09-06 15:10:07 +0200

    successfully changes one dimension

--- src/sammaster/andrebam/DataGenerator.py
+++ src/sammaster/andrebam/DataGenerator.py
@@ -10,14 +10,32 @@ Created on Sep 6, 2013
 --dy=1
 --total_time=5
 '''
-import subprocess #subprocess.call([path, arg, arg ...])
+import commands #subprocess.call([path, arg, arg ...])
+import fileinput
 
 class DataGenerator(object):
-    kp_args = ["--bathymetry_no=6", "--water_elevation_no=5", "--nx=500", "--ny=500", "--dx=1", "--dy=1, --total_time=2"]
+    kp_args = ["--bathymetry_no=6", "--water_elevation_no=5", "--nx=500", "--ny=500", "--dx=1", "--dy=1", "--total_time=2"]
     path = ""
+    max_dim = 5
 
     def __init__(self, _path):
         self.path = _path
+        self.args = [self.path] + self.kp_args
         
     def callLoop(self):
-        print [self.path].append(self.kp_args)
+        for x in range(self.max_dim):
+            print "Running kp with args: "+" ".join(self.args)
+            status, output = commands.getstatusoutput(" ".join(self.args))
+            self.writeData(status, output)
+            for line in fileinput.input("../../../configure (copy).h", inplace=True):
+                if line.find("define KPSIMULATOR_FLUX_BLOCK_WIDTH") >= 0:
+                    print "#define KPSIMULATOR_FLUX_BLOCK_WIDTH "+str(x)+"\n" ,
+                else:
+                    print line,
+        
+    
+    def writeData(self, status, output):
+        lines = output.split("\n")
+        time_taken = lines[-3][14:22]
+        print "Status: "+str(status)+" Time taken: "+time_taken
+        
--- src/sammaster/andrebam/launcher.py
+++ src/sammaster/andrebam/launcher.py
@@ -10,21 +10,30 @@ Take argument, path to KPSImulator
 Start DataGenerator (which runs kp with different blocksizes)
 run GraphGenerator to produce 3d plot of block.x block.y and time taken to run.
 '''
-from sammaster.andrebam import *
+#from sammaster.andrebam import *
+from sammaster.andrebam import DataGenerator
+import os.path
 
 if __name__ == '__main__':
     
     from optparse import OptionParser
     usage = "usage: %prog [var=value]"
     p = OptionParser(usage)
-    p.add_option("--kpPath", type="string", help="path to kp executable with '' - '~/SW-GPU/bin/kp'")
+    p.add_option("--kpPath", type="string", help="path to kp executable with '' - '/SW-GPU/bin/kp'")
     (opts, args) = p.parse_args()
+
     if opts.kpPath == None:
         print "Missing --kpPath argument"
         exit()
-    else:
-        kpPath = opts.kpPath
     
-    gen = DataGenerator(kpPath)
+    print "checking path: "+opts.kpPath
+
+    if os.path.isfile(opts.kpPath):
+        print "file found"
+    else:
+        print "Path "+opts.kpPath+" not found!"
+        exit()
+    
+    gen = DataGenerator.DataGenerator(opts.kpPath)
     gen.callLoop()
     
